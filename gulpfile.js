b40289ffb1e5638029ac95940bee2647c4fc0cff
Stephen Murphy <stephen.jd.murphy@gmail.com>
2015-08-27 16:21:17 +0100

    Added Gulp/ Sass Added some style

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,9 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+
+    mix.styles([
+        'vendor/bootstrap.css',
+        'app.css'
+    ], null, 'public/css')
 });
