5fa4917d05936ca1c5cfc90e645a9df9d157f42d
Stephen Murphy <stephen.jd.murphy@gmail.com>
2015-08-27 16:23:31 +0100

    Fixed issue with the class form-group -> form-control
    Derp

--- resources/views/notices/create.blade.php
+++ resources/views/notices/create.blade.php
@@ -15,22 +15,22 @@
         <!-- infringing_title Form Input -->
         <div class="form-group">
             {!! Form::label('infringing_title', 'What is the title of the content that is being infringed upon') !!}
-            {!! Form::text('infringing_title', null, ['class'=> 'form-group']) !!}
+            {!! Form::text('infringing_title', null, ['class'=> 'form-control']) !!}
         </div>
         <!-- infringing link -->
         <div class="form-group">
             {!! Form::label('infringing_link', 'What is the link to where this content is located') !!}
-            {!! Form::text('infringing_link', null, ['class'=> 'form-group']) !!}
+            {!! Form::text('infringing_link', null, ['class'=> 'form-control']) !!}
         </div>
 
         <div class="form-group">
             {!! Form::label('original_link', 'To verify that you own the content, we now need the link to the original content on your server') !!}
-            {!! Form::text('original_link', null, ['class'=> 'form-group']) !!}
+            {!! Form::text('original_link', null, ['class'=> 'form-control']) !!}
         </div>
 
         <div class="form-group">
             {!! Form::label('original_description', 'And, finally, it might help to provide some extra information related to this DMCA notice') !!}
-            {!! Form::text('original_description', null, ['class'=> 'form-group']) !!}
+            {!! Form::text('original_description', null, ['class'=> 'form-control']) !!}
         </div>
 
 
